6502d3dd6c53de214884739bf28ea538431dc7b2
Matheus Henrique <teus.henrique021@gmail.com>
2026-04-27 16:05:52 -0300

    feat: cria funcionalidades para o administrador

--- model/Administrador.java
+++ model/Administrador.java
@@ -1,5 +1,22 @@
 package model;
 
+import enums.StatusDenuncia;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Administrador extends Pessoa {
     private Boolean nivelAcesso = true;
+
+    public Boolean validarDenuncia(Denuncia denuncia){
+        return true;
+    }
+
+    public void alterarStatus(Denuncia denuncia, StatusDenuncia statusDenuncia){
+        return;
+    }
+
+    public List<Denuncia> listarDenuncia(String filtro){
+        return new ArrayList<>();
+    }
 }
