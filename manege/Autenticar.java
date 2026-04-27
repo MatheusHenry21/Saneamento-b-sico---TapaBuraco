73d773fd785104b22cba5a5d4c68df7c40ca4a38
Matheus Henrique <teus.henrique021@gmail.com>
2026-04-27 15:55:43 -0300

    feat: cria funcoes de autenticacao

---
+++ manege/Autenticar.java
@@ -0,0 +1,17 @@
+package manege;
+
+import model.Pessoa;
+
+public class Autenticar {
+    public Pessoa login(String email, String senha){
+        return new Pessoa();
+    }
+
+    public Pessoa cadastrarPessoa(String dados){
+        return new Pessoa();
+    }
+
+    public Pessoa recuperarSenha(String email){
+        return new Pessoa();
+    }
+}
