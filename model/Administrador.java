94f5d2c8bd9cf5f6733e6974e0250ef25b0f8c95
Matheus Henrique <teus.henrique021@gmail.com>
2026-04-27 16:02:30 -0300

    del: apagar método inutil

--- model/Administrador.java
+++ model/Administrador.java
@@ -2,8 +2,4 @@ package model;
 
 public class Administrador extends Pessoa {
     private Boolean nivelAcesso = true;
-
-    public void gerenciarDenuncia(Denuncia denuncia) {
-        System.out.println("Gerenciando denúncia: " + denuncia);
-    }
 }
