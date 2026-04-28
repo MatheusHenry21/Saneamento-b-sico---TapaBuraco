7afedfeb7c43010dc998737e39b2aa57f8bb62ce
Matheus Henrique <teus.henrique021@gmail.com>
2026-04-28 00:26:09 -0300

    feat: cria menu principal

--- views/Main.java
+++ views/Main.java
@@ -1,13 +1,24 @@
 package views;
 
 import model.*;
+import utils.ScannerUtil;
 
 public class Main {
     public static void main(String[] args) {
-        Denunciante denunciante = new Denunciante();
-        Denuncia denuncia = new Denuncia();
 
-        denuncia.registrarDenuncia();
-        denunciante.enviarDenuncia(denuncia);
+        ScannerUtil entradas = new ScannerUtil();
+        int opcao;
+
+        do {
+            System.out.println("Bem-vindo ao Tapa Buraco\n");
+            System.out.println("---MENU---");
+            System.out.println("1 - Efetuar login");
+            System.out.println("2 - Realizar cadastro");
+            System.out.println("3 - Sair");
+
+            opcao = entradas.opcao();
+
+        }while(opcao != 3);
+
     }
 }
