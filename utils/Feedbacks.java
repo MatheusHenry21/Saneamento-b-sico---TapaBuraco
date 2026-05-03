7aec36815b26a4b219c97967451e4df462637de0
Matheus Henrique <teus.henrique021@gmail.com>
2026-05-03 17:37:37 -0300

    feat: cria feedbacks para o terminal

--- utils/Feedbacks.java
+++ utils/Feedbacks.java
@@ -1,7 +1,21 @@
 package utils;
 
 public class Feedbacks {
-    public void opcaoInvalida(){
-        System.out.println("\nOpção inválida, tente novamente");
+    public void opcaoInvalida(){System.out.println("\nOpção inválida, tente novamente.");}
+
+    public void erroLocalizarEmail(){
+        System.out.println("\nErro, email não encontrado.");
+    }
+
+    public void cadastroSucesso(){
+        System.out.println("\nCadastro realizado com sucesso.");
+    }
+
+    public void alteracaoSucesso(){
+        System.out.println("\nAlteração realizada com sucesso.");
+    }
+
+    public void senhaIncorreta(){
+        System.out.println("\nSenha incorreta.");
     }
 }
